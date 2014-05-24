89c0d293c4ddfff4066784462d9293ac6a5a9c00
Kyle Van Wagenen <kvanwagenen@gmail.com>
2014-05-23 18:43:13 -0600

    Add configuration values for peddler

--- app/models/spree/amazon_payments_configuration.rb
+++ app/models/spree/amazon_payments_configuration.rb
@@ -6,5 +6,8 @@ module Spree
     preference :amazon_mws_endpoint, :string, :default => ''
     preference :widgets_js_url, :string, :default => ''
     preference :profile_api_endpoint, :string, :default => ''
+    preference :marketplace_id, :string, :default => ''
+    preference :aws_access_key_id, :string, :default => ''
+    preference :aws_secret_access_key, :string, :default => ''
   end
 end
