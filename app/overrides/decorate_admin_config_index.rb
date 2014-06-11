29193d1dafa25dd2f969e60d9fa73d6a89c804e5
Kyle Van Wagenen <kvanwagenen@gmail.com>
2014-06-11 11:58:15 -0600

    Remove old admin override

--- app/overrides/decorate_admin_config_index.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
-                     :name => "amazon_payments_configuration_menu",
-                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :text => "<%= configurations_sidebar_menu_item 'Amazon Payments', admin_amazon_payments_settings_path %>",
-                     :disabled => false)
